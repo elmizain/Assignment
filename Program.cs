5db0479448d38a2fae4ec7ed099f1be6ab4559c1
Mahmoud <mahmoudelmizayin@gmail.com>
2026-07-20 01:56:40 +0300

    question 3

--- Program.cs
+++ Program.cs
@@ -18,12 +18,31 @@
                 }
 
                 Console.WriteLine();*/
-                // Question 2
+                /* Question 2
                 bool isAvailable = true;
 
                 if (pages > 300 && isAvailable)
                 {
                     Console.WriteLine("You can borrow this book");
+                }
+
+                Console.WriteLine();*/
+                // Question 3
+                string title = "Refactoring";
+
+                switch (title)
+                {
+                    case "Clean Code":
+                        Console.WriteLine("Great choice!");
+                        break;
+
+                    case "Refactoring":
+                        Console.WriteLine("Nice pick!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Never heard of it");
+                        break;
                 }
 
                 Console.WriteLine();
