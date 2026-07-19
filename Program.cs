ee052a2e3feb7c3e5e919c588c761240dc4a5b09
Mahmoud <mahmoudelmizayin@gmail.com>
2026-07-20 01:40:53 +0300

    question 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-           
+            {
+                // Question 1
+                int pages = 464;
+
+                if (pages > 300)
+                {
+                    Console.WriteLine("Long Book");
+                }
+                else
+                {
+                    Console.WriteLine("Short Book");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
